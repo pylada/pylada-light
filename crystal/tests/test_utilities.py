b6134c632884b30e9c94590571f9e02f703cb27e
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-02-26 17:14:49 +0000

    Adding pure python utilities

--- crystal/tests/test_utilities.py
+++ crystal/tests/test_utilities.py
@@ -22,65 +22,65 @@
 """ Test some utilities from crystal extension module. """
 
 def test_periodic():
-  """ Test periodic images. """
-  from pylada.crystal.cppwrappers import are_periodic_images
-  from numpy import array, dot
-  from numpy.linalg import inv
-  from random import randint, random
-  cell = array([ [0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0] ])
-  invcell = inv(cell)
-  for i in range(10):
-    vec = dot(cell, array([random(), random(), random()]))
-    for j in range(10):
-      vec2 = vec + dot(cell, array([randint(-10, 11), randint(-10, 11), randint(-10, 11)]))
-      assert are_periodic_images(vec, vec2, invcell)
-      vec3 = vec2 + dot(cell, array([random()+0.0001, random(), random()]))
-      assert not are_periodic_images(vec, vec3, invcell)
+    """ Test periodic images. """
+    from pylada.crystal.utilities import are_periodic_images
+    from numpy import array, dot
+    from numpy.linalg import inv
+    from random import randint, random
+    cell = array([ [0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0] ])
+    invcell = inv(cell)
+    for i in range(10):
+        vec = dot(cell, array([random(), random(), random()]))
+        for j in range(10):
+            vec2 = vec + dot(cell, array([randint(-10, 11), randint(-10, 11), randint(-10, 11)]))
+            assert are_periodic_images(vec, vec2, invcell)
+            vec3 = vec2 + dot(cell, array([random()+0.0001, random(), random()]))
+            assert not are_periodic_images(vec, vec3, invcell)
 
 def test_into_cell():
-  """ Test that vector is folded to fractional coordinates >= 0 and < 1. """
-  from pylada.crystal.cppwrappers import are_periodic_images, into_cell
-  from numpy import array, dot, all
-  from numpy.linalg import inv
-  from random import uniform
-  cell = array([ [0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0] ])
-  invcell = inv(cell)
-  for i in range(100):
-    vec = dot(cell, array([uniform(-10, 10), uniform(-10, 10), uniform(-10, 10)]))
-    vec2 = into_cell(vec, cell, invcell)
-    assert are_periodic_images(vec, vec2, invcell)
-    assert all(dot(invcell, vec2) >= 0e0) and all(dot(invcell, vec2) < 1e0)
-  
+    """ Test that vector is folded to fractional coordinates >= 0 and < 1. """
+    from pylada.crystal.utilities import are_periodic_images, into_cell
+    from numpy import array, dot, all
+    from numpy.linalg import inv
+    from random import uniform
+    cell = array([ [0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0] ])
+    invcell = inv(cell)
+    for i in range(100):
+        vec = dot(cell, array([uniform(-10, 10), uniform(-10, 10), uniform(-10, 10)]))
+        vec2 = into_cell(vec, cell, invcell)
+        assert are_periodic_images(vec, vec2, invcell)
+        assert all(dot(invcell, vec2) >= 0e0) and all(dot(invcell, vec2) < 1e0)
+
 def test_zero_centered():
-  """ Test that vector is folded to fractional coordinates >= -0.5 and < 0.5. """
-  from pylada.crystal.cppwrappers import are_periodic_images, zero_centered
-  from numpy import array, dot, all
-  from numpy.linalg import inv
-  from random import uniform
-  cell = array([ [0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0] ])
-  invcell = inv(cell)
-  for i in range(100):
-    vec = dot(cell, array([uniform(-10, 10), uniform(-10, 10), uniform(-10, 10)]))
-    vec2 = zero_centered(vec, cell, invcell)
-    assert are_periodic_images(vec, vec2, invcell)
-    assert all(dot(invcell, vec2) >= -0.5) and all(dot(invcell, vec2) < 0.5)
+    """ Test that vector is folded to fractional coordinates >= -0.5 and < 0.5. """
+    from pylada.crystal.utilities import are_periodic_images, zero_centered
+    from numpy import array, dot, all
+    from numpy.linalg import inv
+    from random import uniform
+    cell = array([ [0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0] ])
+    invcell = inv(cell)
+    for i in range(100):
+        vec = dot(cell, array([uniform(-10, 10), uniform(-10, 10), uniform(-10, 10)]))
+        vec2 = zero_centered(vec, cell, invcell)
+        assert are_periodic_images(vec, vec2, invcell)
+        assert all(dot(invcell, vec2) >= -0.5) and all(dot(invcell, vec2) < 0.5)
 
 def test_into_voronoi():
-  """ Test that vector is folded into first Brillouin zone. """
-  from pylada.crystal.cppwrappers import are_periodic_images, into_voronoi
-  from numpy import array, dot
-  from numpy.linalg import inv
-  from random import uniform
-  cell = array([ [0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0] ])
-  invcell = inv(cell)
-  for i in range(100):
-    vec = dot(cell, array([uniform(-10, 10), uniform(-10, 10), uniform(-10, 10)]))
-    vec2 = into_voronoi(vec, cell, invcell)
-    assert are_periodic_images(vec, vec2, invcell)
-    n = dot(vec2, vec2)
-    assert dot(vec, vec) >= n
-    for j in range(-3, 4):
-      for k in range(-3, 4):
-        for l in range(-3, 4):
-          o = vec2 + dot(cell, [j, k, l])
-          assert dot(o, o) >= n
+    """ Test that vector is folded into first Brillouin zone. """
+    from pylada.crystal.utilities import are_periodic_images, into_voronoi
+    from numpy import array, dot
+    from numpy.linalg import inv
+    from random import uniform
+    cell = array([ [0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0] ])
+    invcell = inv(cell)
+    for i in range(100):
+        vec = dot(cell, array([uniform(-10, 10), uniform(-10, 10), uniform(-10, 10)]))
+        vec2 = into_voronoi(vec, cell, invcell)
+        assert are_periodic_images(vec, vec2, invcell)
+        n = dot(vec2, vec2)
+        assert dot(vec, vec) >= n
+        for j in range(-3, 4):
+            for k in range(-3, 4):
+                for l in range(-3, 4):
+                    o = vec2 + dot(cell, [j, k, l])
+                    assert dot(o, o) >= n
---
+++ crystal/utilities.py
@@ -0,0 +1,103 @@
+def into_cell(position, cell, inverse=None):
+    """ Returns Folds vector into periodic the input cell
+
+        :param position:
+            Vector/position to fold back into the cell
+        :param cell:
+            The cell matrix defining the periodicity
+        :param invcell:
+            Optional. The *inverse* of the cell defining the periodicity. It is
+            computed if not given on input.
+    """
+    from numpy import dot, floor
+    from numpy.linalg import inv
+    if inverse is None:
+        inverse = inv(cell)
+    result = dot(inverse, position)
+    return dot(cell, result - floor(result))
+
+def zero_centered(position, cell, inverse=None):
+    """ Folds vector back to origin
+
+        This may not be the vector with the smallest possible norm if the cell
+        is very skewed
+
+        :param position:
+            Vector/position to fold back into the cell
+        :param cell:
+            The cell matrix defining the periodicity
+        :param invcell:
+            Optional. The *inverse* of the cell defining the periodicity. It is
+            computed if not given on input.
+    """
+    from numpy import dot, floor, abs
+    from numpy.linalg import inv
+    if inverse is None:
+        inverse = inv(cell)
+    result = dot(inverse, position)
+    result -= floor(result + 0.5 + 1e-12)
+    for i in range(result.size):
+        if abs(result[i] - 0.5) < 1e-12:
+            result[i] = -0.5
+        elif result[i] < -0.5:
+            result[i] += 1e0
+    return dot(cell, result)
+
+def into_voronoi(position, cell, inverse=None):
+    """ Folds vector into first Brillouin zone of the input cell
+
+        Returns the periodic image with the smallest possible norm.
+
+        :param position:
+            Vector/position to fold back into the cell
+        :param cell:
+            The cell matrix defining the periodicity
+        :param invcell:
+            Optional. The *inverse* of the cell defining the periodicity. It is
+            computed if not given on input.
+    """
+    from numpy import dot, floor
+    from numpy.linalg import inv, norm
+    if inverse is None:
+        inverse = inv(cell)
+    center = dot(inverse, position)
+    center -= floor(center)
+
+    result = center
+    n = norm(center);
+    for i in range(-1, 2, 1):
+        for j in range(-1, 2, 1):
+            for k in range(-1, 2, 1):
+                translated = [i, j, k] + center
+                n2 = norm(dot(cell, translated))
+                if n2 < n:
+                    n = n2
+                    result = translated
+    return dot(cell, result)
+
+def are_periodic_images(pos0, pos1, invcell=None, cell=None, tolerance=1e-8):
+    """ True if two vector are periodic images of one another
+
+        :param pos0:
+            First position
+        :param pos1:
+            Second position
+        :param invcell:
+            The *inverse* of the cell defining the periodicity
+        :param cell:
+            The cell defining the periodicity
+        :param tolerance:
+            Fuzzy tolerance criteria
+
+        Only one of cell and invcell need be given.
+    """
+    from numpy import dot, floor
+    from numpy.linalg import inv, norm
+    if invcell is None:
+        if cell is None:
+            raise ValueError("One of cell or invcell should be given")
+        invcell = inv(cell)
+
+    result = dot(invcell, pos0 - pos1)
+    result -= floor(result + 1e-12)
+    return all(abs(result) < tolerance)
