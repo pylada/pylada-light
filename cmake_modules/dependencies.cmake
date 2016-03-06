04ffec77c0dc1d7dc00b0a5f0cc55581cab8dda9
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-03-05 17:09:44 -0700

    Update dependencies

--- cmake_modules/dependencies.cmake
+++ cmake_modules/dependencies.cmake
@@ -4,27 +4,23 @@ lookup_package(Eigen3 REQUIRED)
 
 find_package(CoherentPython REQUIRED)
 include(PythonPackage)
-function(find_or_fail package)
-    find_python_package(${package})
-    if(NOT ${package}_FOUND)
-        message("*********")
-        message("${package} required")
-        message("It can likely be installed with pip")
-        message("*********")
-        message(FATAL_ERROR "Aborting")
-    endif()
-endfunction()
-
-# first looks for python package, second for linkage/include stuff
-find_or_fail(numpy)
+include(PythonPackageLookup)
+find_python_package(numpy)
+find_python_package(quantities)
+# only needed for build. So can install it locally in build dir.
+lookup_python_package(cython)
+# Finds additional info, like libraries, include dirs...
+# no need check numpy features, it's all handled by cython.
+set(no_numpy_feature_tests TRUE)
 find_package(Numpy REQUIRED)
-find_or_fail(quantities)
 
 # Create local python environment
 # If it exists, most cookoff functions will use LOCAL_PYTHON_EXECUTABLE rather
 # than PYTHON_EXECUTABLE. In practice, this means that packages installed in
 # the build tree can be found.
 include(EnvironmentScript)
+add_to_python_path("${PROJECT_BINARY_DIR}/python")
+add_to_python_path("${EXTERNAL_ROOT}/python")
 set(LOCAL_PYTHON_EXECUTABLE "${PROJECT_BINARY_DIR}/localpython.sh")
 create_environment_script(
     PYTHON
@@ -33,8 +29,6 @@ create_environment_script(
 )
 
 if(tests)
-    include(PythonPackageLookup)
-    add_to_python_path("${EXTERNAL_ROOT}/python")
     lookup_python_package(pytest)
     # Not required per se but usefull for testing process
     find_python_package(mpi4py)
