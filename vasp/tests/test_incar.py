edee3c559ecdb08dae9e112b3c423079009fa5f0
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-03-09 11:22:22 -0700

    Spruced vasp incar tests

--- vasp/tests/test_incar.py
+++ vasp/tests/test_incar.py
@@ -19,19 +19,22 @@
 #  You should have received a copy of the GNU General Public License along with PyLaDa.  If not, see
 #  <http://www.gnu.org/licenses/>.
 ###############################
+from pytest import fixture
 
 
-def test_incar():
-    from shutil import rmtree
-    from tempfile import mkdtemp
-    from os.path import join, dirname
-    from quantities import eV
-    from pylada.vasp import Vasp, read_incar
+@fixture
+def structure():
     from pylada.crystal import Structure
-    structure = Structure([[0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0]], scale=5.43, name='has a name')\
+    cell = [[0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0]]
+    return Structure(cell, scale=5.43, name='has a name')\
         .add_atom(0, 0, 0, "Si")\
         .add_atom(0.25, 0.25, 0.25, "Si")
 
+
+@fixture
+def vasp():
+    from os.path import join, dirname
+    from pylada.vasp import Vasp
     vasp = Vasp()
     vasp.kpoints = "Automatic generation\n0\nMonkhorst\n2 2 2\n0 0 0"
     vasp.precision = "accurate"
@@ -41,41 +44,41 @@ def test_incar():
     vasp.sigma = 0.06
     vasp.relaxation = "volume"
     vasp.add_specie = "Si", join(dirname(__file__), 'pseudos', 'Si')
+    return vasp
 
-    directory = mkdtemp()
-    try:
-        vasp.write_incar(path=join(directory, 'INCAR'), structure=structure)
-        other = read_incar(join(directory, 'INCAR'))
-        assert abs(other.ediff - 1e-5) < 1e-8
-        assert abs(other.encut - 245.345) < 1e-8
-        assert abs(other.sigma - 0.06 * eV) < 1e-8
-        assert other.ibrion == 2
-        assert other.icharg == 'atomic'
-        assert other.isif == 7
-        assert other.ismear == 'metal'
-        assert other.istart == 'scratch'
-        assert other.lcharg == False
-        assert other.nsw == 50
-        assert other.relaxation == 'volume'
-        assert other.system == 'has a name'
-        with open(join(directory, 'INCAR'), 'a') as file:
-            file.write('\nSOMETHing = 0.5\n')
-        other = read_incar(join(directory, 'INCAR'))
-        assert abs(other.ediff - 1e-5) < 1e-8
-        assert abs(other.encut - 245.345) < 1e-8
-        assert abs(other.sigma - 0.06 * eV) < 1e-8
-        assert other.ibrion == 2
-        assert other.icharg == 'atomic'
-        assert other.isif == 7
-        assert other.ismear == 'metal'
-        assert other.istart == 'scratch'
-        assert other.lcharg == False
-        assert other.nsw == 50
-        assert other.relaxation == 'volume'
-        assert other.system == 'has a name'
-        assert 'something' in other._input
-        assert isinstance(other.something, float)
-        assert abs(other.something - 0.5) < 1e-8
-    finally:
-        rmtree(directory)
-        pass
+
+def check_vasp(other):
+    from quantities import eV
+    assert abs(other.ediff - 1e-5) < 1e-8
+    assert abs(other.encut - 245.345) < 1e-8
+    assert abs(other.sigma - 0.06 * eV) < 1e-8
+    assert other.ibrion == 2
+    assert other.icharg == 'atomic'
+    assert other.isif == 7
+    assert other.ismear == 'metal'
+    assert other.istart == 'scratch'
+    assert other.lcharg == False
+    assert other.nsw == 50
+    assert other.relaxation == 'volume'
+    assert other.system == 'has a name'
+
+
+def test_read_from_incar(tmpdir, vasp, structure):
+    from pylada.vasp import read_incar
+    vasp.write_incar(path=str(tmpdir.join('INCAR')), structure=structure)
+    print(tmpdir.join('INCAR').read())
+    other = read_incar(str(tmpdir.join('INCAR')))
+    check_vasp(other)
+
+
+def test_read_from_incar_with_unknowns_params(tmpdir, vasp, structure):
+    from pylada.vasp import read_incar
+    vasp.write_incar(path=str(tmpdir.join('INCAR')), structure=structure)
+    with open(str(tmpdir.join('INCAR')), 'a') as file:
+        file.write('\nSOMETHing = 0.5\n')
+
+    other = read_incar(str(tmpdir.join('INCAR')))
+    check_vasp(other)
+    assert 'something' in other._input
+    assert isinstance(other.something, float)
+    assert abs(other.something - 0.5) < 1e-8
