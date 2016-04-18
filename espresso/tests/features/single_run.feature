5bcdde30bcf96a7a65b4a0ebb10aeb2b5723b3fb
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-04-18 21:31:44 +0100

    Bdd for iterating through first step of pwscf.iter

--- espresso/tests/features/single_run.feature
+++ espresso/tests/features/single_run.feature
@@ -4,8 +4,8 @@ Feature: single run
 
 Scenario: Writing a pwscf input file
 
-    pwscf.system.ecutwfc is the one parameter that must be set prior to calling the functional (or
-    writing it's input)
+        pwscf.system.ecutwfc is the one parameter that must be set prior to calling the functional
+        (or writing it's input)
 
     Given a Pwscf object
     And mandatory attribute pwscf.system.ecutwfc is set to 14.0 Ry
@@ -16,10 +16,10 @@ Scenario: Writing a pwscf input file
 
 Scenario: Writing a pwscf input file with an unknown parameter
 
-    All attributes in the pwscf namelists are automatically written to file, even those that have
-    not yet been setup in pylada. This means any keyword needed by pwscf can be added without
-    changing the code. The only restrictions are the allowed types: string, integers, floating
-    points and arrays of integers and floating points
+        All attributes in the pwscf namelists are automatically written to file, even those that
+        have not yet been setup in pylada. This means any keyword needed by pwscf can be added
+        without changing the code. The only restrictions are the allowed types: string, integers,
+        floating points and arrays of integers and floating points
 
     Given a Pwscf object
     And mandatory attribute pwscf.system.ecutwfc is set to 14.0 Ry
@@ -29,3 +29,28 @@ Scenario: Writing a pwscf input file with an unknown parameter
     And wavefunction cutoff is equal to 14.0 Ry
     And pwscf.electrons.whawha is equal to 1.5
 
+
+Scenario: Launching a (fake) pwscf calculation and check input
+
+        Creates and launches a calculation. However, we do not actually launch the calculation.
+        Instead we check that the functional is an iterator with two steps:
+
+        - the first step yields a ProgrammProcess object that can execute an external binary
+        - the second step yields an extraction object
+
+        Calling pwscf directly (`pwscf(structure, outdir)`) is equivalent to iterating through the
+        first step, executing it, and then returning the value of the second step.
+        At the first iteration, all the input for pwscf should be present.
+
+    Given a pwscf object setup as follows
+        pwscf.system.ecutwfc = 12.0*Ry
+        pwscf.kpoints.subtitle = None
+        pwscf.kpoints.value = "2\n"\
+            "0.25 0.25 0.75 3.0\n"\
+            "0.25 0.25 0.25 1.0\n"
+        pwscf.add_specie('Al', 'Al.pz-vbc.UPF')
+    And a fake pseudo 'Al.pz-vbc.UPF' in the working directory
+    And an aluminum structure
+    When iterating through the first step
+    Then the yielded object is a ProgrammProcess
+    And a valid pwscf.in exists
--- espresso/tests/test_single_run.py
+++ espresso/tests/test_single_run.py
@@ -5,27 +5,27 @@ scenarios("features/single_run.feature")
 
 
 @given("a Pwscf object")
-def pwscf():
+def empty_pwscf():
     from pylada.espresso import Pwscf
     return Pwscf()
 
 
 @given(parsers.parse("mandatory attribute pwscf.system.ecutwfc is set to {value:f} {units:S}"))
-def set_ecutwfc(pwscf, value, units):
+def set_ecutwfc(empty_pwscf, value, units):
     import quantities
-    pwscf.system.ecutwfc = value * getattr(quantities, units)
+    empty_pwscf.system.ecutwfc = value * getattr(quantities, units)
 
 
 @given(parsers.parse("pwscf.{namelist:w}.{attribute:w} is set to {value:f}"))
-def set_an_attribute0(pwscf, namelist, attribute, value):
+def set_an_attribute0(empty_pwscf, namelist, attribute, value):
     # equivalent to pwscf.namelist.attribute = value
     # with namelist and attribute replaced by their values
-    setattr(getattr(pwscf, namelist), attribute, value)
+    setattr(getattr(empty_pwscf, namelist), attribute, value)
 
 
 @when(parsers.parse("writing to {filename:S} without specifying a structure"))
-def writing_no_structure(pwscf, tmpdir, filename):
-    pwscf.write(tmpdir.join(filename))
+def writing_no_structure(empty_pwscf, tmpdir, filename):
+    empty_pwscf.write(tmpdir.join(filename))
 
 
 @then(parsers.parse("the file {filename:S} appears and can be read"))
@@ -53,3 +53,55 @@ def check_ecutwfc(pwscf_out, value, units):
     from numpy import allclose
     assert pwscf_out.system.ecutwfc.units == getattr(quantities, units)
     assert allclose(float(pwscf_out.system.ecutwfc), value)
+
+
+@given(parsers.parse("a pwscf object setup as follows\n{text}"))
+def pwscf(text):
+    from quantities import Ry
+    from pylada.espresso import Pwscf
+    pwscf = Pwscf()
+    exec(text, globals(), {'pwscf': pwscf, 'Ry': Ry})
+    return pwscf
+
+
+@given(parsers.parse("a fake pseudo '{filename}' in the working directory"))
+def pseudo_filename(tmpdir, filename):
+    tmpdir.join(filename).ensure(file=True)
+    return tmpdir.join(filename)
+
+
+@given("an aluminum structure")
+def aluminum():
+    from quantities import bohr_radius
+    from pylada.crystal.bravais import fcc
+    result = fcc()
+    result.scale = 7.5 * bohr_radius
+    result[0].type = 'Al'
+    return result
+
+
+@pytest.fixture
+def passon():
+    """ A container to pass information from when to then """
+    return []
+
+
+@when("iterating through the first step")
+def first_step(pwscf, tmpdir, aluminum, passon):
+    iterator = pwscf.iter(aluminum, tmpdir)
+    passon.extend([iterator, iterator.next()])
+
+
+@then("the yielded object is a ProgrammProcess")
+def first_yield(passon):
+    from pylada.process import ProgramProcess
+    iterator, first_step = passon
+    assert isinstance(first_step, ProgramProcess)
+
+
+@then(parsers.parse("a valid {filename} exists"))
+def check_pwscf_input(tmpdir, filename, pwscf):
+    actual = read_pwscf(tmpdir, filename)
+    assert abs(actual.system.ecutwfc - pwscf.system.ecutwfc) < 1e-8
+    assert actual.kpoints.subtitle == pwscf.kpoints.subtitle
+    assert actual.kpoints.value.rstrip().lstrip() == pwscf.kpoints.value.rstrip().lstrip()
