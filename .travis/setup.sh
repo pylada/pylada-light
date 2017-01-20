cf6e8f72964b48c16972835553a7e432a768a6d4
Mayeul d'Avezac <mdavezac@gmail.com>
2017-01-20 14:42:58 +0000

    Adding no cache option to pip

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,8 +2,8 @@
 sudo apt-get install build-essential
 sudo apt-get install -q gfortran openmpi-bin openmpi-common libopenmpi-dev
 sudo apt-get install python-scipy python-numpy
-sudo pip install --upgrade pip
-sudo pip install --upgrade setuptools
+sudo pip install --no-cache --upgrade setuptools
+sudo pip install --no-cache --upgrade pip
 sudo pip install --upgrade numpy
 sudo pip install --no-cache ipython
 sudo pip install --no-cache quantities
@@ -11,8 +11,9 @@ sudo pip install --no-cache mpi4py
 sudo pip install --no-cache six
 sudo pip install --no-cache f90nml
 sudo pip install --no-cache ipython
-sudo pip install pytest pytest_bdd
-sudo pip install -q Cython
+sudo pip install --no-cache pytest pytest_bdd
+sudo pip install --no-cache -q Cython
+sudo pip install --no-cache runipy
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get install -qq g++-4.8
