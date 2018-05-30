4d9b6802343400f1f285d14882b66daeed36279e
Mayeul d'Avezac <mdavezac@gmail.com>
2018-05-30 17:48:25 +0100

    Problems with travis...

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,19 +1,20 @@
 #! /usr/bin/env bash
+sudo apt-get update
 sudo apt-get install build-essential
-sudo apt-get install -q gfortran openmpi-bin openmpi-common libopenmpi-dev
+sudo apt-get install -q gfortran openmpi-bin openmpi-common libopenmpi-dev gcc-g++ cmake
 sudo apt-get install python-scipy python-numpy
 sudo pip install --no-cache --upgrade setuptools
 sudo pip install --no-cache --upgrade pip
 sudo pip install --upgrade numpy
-sudo pip install --no-cache ipython
-sudo pip install --no-cache quantities
-sudo pip install --no-cache mpi4py
-sudo pip install --no-cache six
-sudo pip install --no-cache f90nml
-sudo pip install --no-cache ipython
-sudo pip install --no-cache pytest pytest_bdd
-sudo pip install --no-cache -q Cython
-sudo pip install --no-cache runipy
+sudo pip install --upgrade --no-cache quantities
+sudo pip install --upgrade --no-cache mpi4py
+sudo pip install --upgrade --no-cache six
+sudo pip install --upgrade --no-cache f90nml
+sudo pip install --upgrade --no-cache ipython[all]
+sudo pip install --upgrade --no-cache pytest pytest_bdd
+sudo pip install --upgrade --no-cache -q Cython
+sudo pip install --upgrade --no-cache runipy
+sudo pip install --upgrade --no-cache traitlets
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get install -qq g++-4.8
