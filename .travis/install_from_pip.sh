da1f48b537eac8f12c16a9b21acfae826f70ad16
Mayeul d'Avezac <mdavezac@gmail.com>
2018-06-09 22:57:35 +0100

    Call pip through python

--- .travis/install_from_pip.sh
+++ .travis/install_from_pip.sh
@@ -6,7 +6,7 @@ cat > install_from_pip.sh << EOF
 set -e
 set -o
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
-  pip install --user git+git://github.com/$TRAVIS_REPO_SLUG@$TRAVIS_COMMIT
+  python3 -m pip install --user git+git://github.com/$TRAVIS_REPO_SLUG@$TRAVIS_COMMIT
   python -c "import pylada; pylada.test()"
 fi
 EOF
