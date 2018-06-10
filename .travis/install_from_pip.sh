58b4db4f40ea8e0f928b2b559e7b53826b833206
Mayeul d'Avezac <mdavezac@gmail.com>
2018-06-10 10:18:19 +0100

    Okay, now half-half

--- .travis/install_from_pip.sh
+++ .travis/install_from_pip.sh
@@ -2,18 +2,7 @@
 set -e
 set -o
 
-cat > install_from_pip.sh << EOF
-#!/bin/bash
 source ~/.bashrc
-set -e
-set -o
-if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
-  python3 -m pip install --user git+git://github.com/$TRAVIS_REPO_SLUG@$TRAVIS_COMMIT
-  python -c "import pylada; pylada.test()"
-fi
-EOF
-chmod u+x install_from_pip.sh
-
-docker run -it --rm -v $(pwd):/project -w /project \
-        --env "CC=$ccomp" --env "CXX=$cxxcomp" --cap-add SYS_PTRACE \
-        ./install_from_pip.sh
+module load mpi
+python3 -m pip install -e .
+python3 -c "from pylada import tests; test()"
