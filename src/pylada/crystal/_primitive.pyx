81fc11628243c4ff6db4e84a6e01183e66e461f1
ftherrien <felix.therrien@gmail.com>
2021-08-31 17:03:30 -0400

    Added a recursive mode to primitive that can handle much larger structures

--- src/pylada/crystal/_primitive.pyx
+++ src/pylada/crystal/_primitive.pyx
@@ -20,10 +20,10 @@
 #  <http://www.gnu.org/licenses/>.
 ###############################
 
-cdef __translations(structure, double tolerance):
+def __translations(structure, double tolerance):
     """ Looks for internal translations """
     from numpy.linalg import inv
-    from numpy import all, abs, allclose
+    from numpy import all, abs, allclose, mod
     from . import gruber, into_cell, into_voronoi
 
     cell = gruber(structure.cell)
@@ -32,32 +32,36 @@ cdef __translations(structure, double tolerance):
     front_type = structure[0].type
     center = structure[0].pos
     translations = []
+    
     for site in structure:
         if front_type != site.type:
             continue
 
-        translation = into_voronoi(site.pos - center, cell, invcell)
+        translation = into_voronoi(site.pos - center + tolerance, cell, invcell) - tolerance
+        
         if all(abs(translation) < tolerance):
             continue
 
         for mapping in structure:
-            pos = into_cell(mapping.pos + translation, cell, invcell)
+            pos = into_cell(mapping.pos + translation + tolerance, cell, invcell) - tolerance
             for mappee in structure:
-                if mapping.type == mappee.type and allclose(mappee.pos, pos, tolerance):
+                if mapping.type == mappee.type and allclose(into_cell(mappee.pos + tolerance, cell, invcell)-tolerance, pos, atol=tolerance):
                     break
             else:
                 break
         else:
-            translations.append(into_voronoi(translation, cell, invcell))
+            yield into_voronoi(translation, cell, invcell)
 
-    return translations
+    yield cell[:, 0]
+    yield cell[:, 1]
+    yield cell[:, 2]
 
 
-def primitive(structure, double tolerance=1e-8):
+def primitive(structure, double tolerance=1e-8, bint recursive=False):
     """ Tries to compute the primitive cell of the input structure """
     # The tolerance is the absolute tolerance on the translations
     from numpy.linalg import inv, det
-    from numpy import all, abs, array, dot, allclose, round
+    from numpy import all, abs, array, dot, allclose, round, mod
     from . import gruber, into_cell, into_voronoi, into_cell
     from .. import error, logger
 
@@ -68,32 +72,29 @@ def primitive(structure, double tolerance=1e-8):
     cell = gruber(result.cell)
     invcell = inv(cell)
     for atom in result:
-        atom.pos = into_cell(atom.pos, cell, invcell)
+        atom.pos = into_cell(atom.pos + tolerance, cell, invcell) - tolerance
 
-    translations = __translations(result, tolerance)
-    if len(translations) == 0:
+    for i,t in enumerate(__translations(result, tolerance)):
+        if i > 2:
+            break
+    else:
         logger.debug("Found no inner translations: structure is primitive")
         for a, b in zip(result, structure):
             a.pos[:] = b.pos[:]
         return result
-
-    # adds original translations.
-    translations.append(cell[:, 0])
-    translations.append(cell[:, 1])
-    translations.append(cell[:, 2])
-
+    
     # Looks for cell with smallest volume 
     new_cell = result.cell.copy()
     volume = abs(det(new_cell))
-    for i, first in enumerate(translations):
-        for j, second in enumerate(translations):
+    for i, first in enumerate(__translations(result, tolerance)):
+        for j, second in enumerate(__translations(result, tolerance)):
             if i == j:
                 continue
-            for k, third in enumerate(translations):
+            for k, third in enumerate(__translations(result, tolerance)):
                 if i == k or j == k:
                     continue
                 trial = array([first, second, third]).T
-                if abs(det(trial)) < abs(volume)/len(structure)*(1 - 3*tolerance): # The volume of the cell cannot be smaller than the specific volume 
+                if abs(det(trial)) < abs(result.volume)/len(structure)*(1 - 3*tolerance): # The volume of the cell cannot be smaller than the specific volume
                     continue
                 if abs(det(trial)) > volume*(1 - 3.0 * tolerance):
                     continue
@@ -106,30 +107,39 @@ def primitive(structure, double tolerance=1e-8):
                         logger.error(msg)
                         raise error.RuntimeError(msg)
                 integer_cell = dot(inv(trial), cell)
-                if allclose(integer_cell, round(integer_cell + 1e-7), tolerance): # This needs to be a supercell within the specified tolerance
+                if allclose(integer_cell, round(integer_cell + 1e-7), atol = tolerance): # This needs to be a supercell within the specified tolerance
                     new_cell = trial
                     volume = abs(det(trial))
-
+                    if recursive:
+                        break
+            else:
+                continue
+            break
+        else:
+            continue
+        break
+            
     # Found the new cell with smallest volume (e.g. primivite)
     if abs(structure.volume - volume) < 1e-12: # It actually did not change
         msg = "Found translation but no primitive cell."
         logger.error(msg)
         raise error.RuntimeError(msg)
-
+    
     # now creates new lattice.
     result.clear()
     logger.debug("Found potential cell {!r}".format(new_cell))
     result.cell = gruber(new_cell)
     invcell = inv(result.cell)
     for site in structure:
-        pos = into_cell(site.pos, result.cell, invcell)
+        pos = result.cell.dot(mod(invcell.dot(site.pos) + tolerance, 1) - tolerance)
+        # pos = into_cell(site.pos + tolerance, result.cell, invcell)-tolerance
         for unique in result:
-            if site.type == unique.type and allclose(unique.pos, pos, abs(structure.volume / result.volume) * tolerance): # The difference between pos and unique pos is that of site.pos and the image of unique.pos
+            if site.type == unique.type and allclose(unique.pos, pos, atol = abs(structure.volume / result.volume) * tolerance): # The difference between pos and unique pos is that of site.pos and the image of unique.pos
                 break
         else:
             result.append(site.copy())
             result[-1].pos = pos
-
+            
     if len(structure) % len(result) != 0:
         msg = "Nb of atoms in output not multiple of input."
         logger.error(msg)
@@ -141,7 +151,11 @@ def primitive(structure, double tolerance=1e-8):
         raise error.RuntimeError(msg)
 
     logger.debug("Primitive structure found with %i/%i atoms" % (len(result), len(structure)))
-    return result;
+
+    if recursive:
+        return primitive(result, tolerance=tolerance, recursive=True)
+    else:
+        return result
 
 def is_primitive(structure, double tolerance = 1e-12):
     """ True if the lattice is primitive
@@ -165,4 +179,8 @@ def is_primitive(structure, double tolerance = 1e-12):
     for atom in result:
         atom.pos = into_cell(atom.pos, cell, invcell)
 
-    return len(__translations(result, tolerance)) == 0
+    for i,t in enumerate(__translations(result, tolerance)):
+        if i > 2:
+            return False
+    else:
+        return True
